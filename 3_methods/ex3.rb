960b5cba962345a34e40cffe814410fd5be08537
Raul Romero <raulromerocano@aggienetwork.com>
2020-06-11 10:16:06 +0000

    Chapter 3 exercises

---
+++ 3_methods/ex3.rb
@@ -0,0 +1,10 @@
+puts "Select first number: "
+x = gets.chomp.to_i
+puts "Select second number: "
+y = gets.chomp.to_i
+
+def multiply(x, y)
+  x * y
+end
+
+puts multiply(x, y)
---
+++ 3_methods/ex4.rb
@@ -0,0 +1,11 @@
+def scream(words)
+  words = words + "!!!!"
+  x = 5
+  #return
+  #puts words
+end
+
+puts scream("Yippeee")
+
+# Yippeee
+# 
